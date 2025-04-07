3078d99abd9412900361e1a1c85cdc3ea2d8e5d6
IRSHAD1119 <irshadrazaq11@gmail.com>
2025-04-06 21:44:45 -0700

    Add files via upload

---
+++ classdog.java
@@ -0,0 +1,27 @@
+package lab2;
+
+public class classdog {
+	String name;
+	int age;
+	void setDog(String n,int a) {
+		name=n;
+		age=a;
+}
+	public static void main(String a[]) {
+	    classdog d1=new classdog();
+		System.out.println("d1 dog object");
+		System.out.println(d1.name);
+		System.out.println(d1.age);
+        d1.Set
+        .Classdog("snoofy",2);
+        System.out.println(d1.name);
+        System.out.println(d1.age);
+        
+        classdog d2=new classdog();
+        System.out.println("d2 dog object");
+        System.out.println(d2.name);
+        System.out.println(d2.age);
+	}
+}
+        
+        
